de91129b3f809f1f723df8cf42020a1a11b9a847
lnarayan-main <laxmi.narayan.main1@gmail.com>
2025-11-19 12:07:04 +0530

    updated

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,7 +1,9 @@
 import { defineStore } from 'pinia'
+import config from "@/config";
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL
+// const BASE_URL = import.meta.env.VITE_API_URL
+const BASE_URL = config.API_BASE_URL
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({

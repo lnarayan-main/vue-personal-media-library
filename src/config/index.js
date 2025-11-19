8c240cc7730d08df224e5c32c38b9dce0a26117b
lnarayan-main <laxmi.narayan.main1@gmail.com>
2025-11-19 12:10:35 +0530

    updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const config = {
     // API_BASE_URL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
-    API_BASE_URL: import.meta.env.VITE_API_URL || "https://mediahub-backend-9vwj.onrender.com/",
+    // API_BASE_URL: import.meta.env.VITE_API_URL || "https://mediahub-backend-9vwj.onrender.com/",
+    API_BASE_URL: "https://mediahub-backend-9vwj.onrender.com/",
 
     // FEATURE_FLAG_PROFILES: import.meta.env.VITE_ENABLE_PROFILES === 'true',
 };
